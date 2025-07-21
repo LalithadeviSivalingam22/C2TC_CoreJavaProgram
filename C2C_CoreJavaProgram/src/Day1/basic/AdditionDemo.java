a4f201668192f6d3099e6007ff5d5d0c9826ac45
slali <slali@Lalithadevisivalingam>
2025-07-21 15:26:13 +0530

    This is my first project

---
+++ C2C_CoreJavaProgram/src/Day1/basic/AdditionDemo.java
@@ -0,0 +1,17 @@
+package Day1.basic;
+import java.util.*;
+public class AdditionDemo {
+
+	public static void main(String[] args) {
+		   String a,b,c;
+	       Scanner sc=new Scanner(System.in);
+	       System.out.println("Enter the values:");
+	       a=sc.nextLine();
+	       b=sc.nextLine();
+	       c=a+b;
+	       System.out.println("Addition of two number="+c);
+		}
+
+	}
+
+
