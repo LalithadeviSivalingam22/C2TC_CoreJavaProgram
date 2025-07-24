8360500b636eb8125d6536a530d3ab29dbef76d2
slali <slali@Lalithadevisivalingam>
2025-07-24 21:05:56 +0530

    Da

---
+++ C2C_CoreJavaProgram/src/dayfive/multilevelinheritance/vechicle/Car.java
@@ -0,0 +1,11 @@
+package dayfive.multilevelinheritance.vechicle;
+public class Car {
+	public Car() {
+		System.out.println("Class Car");
+	}
+
+	public void vehicleType() {
+		System.out.println("Vehicle Type: Car");
+	}
+
+}
---
+++ C2C_CoreJavaProgram/src/dayfive/multilevelinheritance/vechicle/Maruti.java
@@ -0,0 +1,13 @@
+package dayfive.multilevelinheritance.vechicle;
+public class Maruti extends Car {
+	public Maruti() {
+		System.out.println("Class Maruti");
+	}
+
+	public void brand() {
+		System.out.println("Brand: Maruti");
+	}
+	public void speed() {
+		System.out.println("Max: 90Kmph");
+	}
+}
