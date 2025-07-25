3aa15cddeebf22d2b88d8a7653673421af894bee
slali <slali@Lalithadevisivalingam>
2025-07-25 14:51:51 +0530

    day 6

---
+++ C2C_CoreJavaProgram/src/day6/staticblockmethod/MyClassDemo.java
@@ -0,0 +1,10 @@
+package day6.staticblockmethod;
+
+public class MyClassDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ C2C_CoreJavaProgram/src/day6/usingfinal/FinalVariable.java
@@ -0,0 +1,10 @@
+package day6.usingfinal;
+
+public class FinalVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
