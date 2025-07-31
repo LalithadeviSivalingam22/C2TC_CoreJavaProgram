a75c7ed73c0c6ef0bd11b4ccd79d97cc1996df3a
slali <slali@Lalithadevisivalingam.mshome.net>
2025-07-31 14:28:16 +0530

    DAY 11

---
+++ C2C_CoreJavaProgram/src/day11/v1/Demo.java
@@ -0,0 +1,10 @@
+package day11.v1;
+//Program without exception handling
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("the program continues....");
+		int data=100/0;
+	}
+	
+}
